91c3667dab13f2125a8b71a394f02a2739a9e4d2
Tin Goricki <tin.fabijan.goricki@vuv.hr>
2023-12-28 23:16:12 +0100

    Added relative path

--- Program.cs
+++ Program.cs
@@ -161,20 +161,63 @@ namespace Natjecanje_u_programiranju
 
             
         }
+
+        static void kreirajNatjecatelje()
+        {
+            string nazivDatoteke = "natjecatelji.json";
+            string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, nazivDatoteke);
+
+            Natjecatelj natjecatelj01 = new Natjecatelj(Guid.NewGuid(), "Marko");
+            Natjecatelj natjecatelj02 = new Natjecatelj(Guid.NewGuid(), "Dominik");
+            Natjecatelj natjecatelj03 = new Natjecatelj(Guid.NewGuid(), "Petar");
+            Natjecatelj natjecatelj04 = new Natjecatelj(Guid.NewGuid(), "Ana");
+            Natjecatelj natjecatelj05 = new Natjecatelj(Guid.NewGuid(), "Mario");
+            Natjecatelj natjecatelj06 = new Natjecatelj(Guid.NewGuid(), "Luka");
+            Natjecatelj natjecatelj07 = new Natjecatelj(Guid.NewGuid(), "Barbara");
+            Natjecatelj natjecatelj08 = new Natjecatelj(Guid.NewGuid(), "Antonio");
+            Natjecatelj natjecatelj09 = new Natjecatelj(Guid.NewGuid(), "Domagoj");
+            Natjecatelj natjecatelj10 = new Natjecatelj(Guid.NewGuid(), "Antonela");
+            Natjecatelj natjecatelj11 = new Natjecatelj(Guid.NewGuid(), "Rudi");
+            Natjecatelj natjecatelj12 = new Natjecatelj(Guid.NewGuid(), "Tomislav");
+            Natjecatelj natjecatelj13 = new Natjecatelj(Guid.NewGuid(), "Karlo");
+            Natjecatelj natjecatelj14 = new Natjecatelj(Guid.NewGuid(), "Fran");
+            Natjecatelj natjecatelj15 = new Natjecatelj(Guid.NewGuid(), "Iva");
+
+            List<Natjecatelj> lNatljecatelja = new List<Natjecatelj> { natjecatelj01, natjecatelj02, natjecatelj03, natjecatelj04, natjecatelj05, natjecatelj06, natjecatelj07, natjecatelj08, natjecatelj09, natjecatelj10, natjecatelj11, natjecatelj12, natjecatelj13, natjecatelj14, natjecatelj15 };
+
+            string noviJson = JsonConvert.SerializeObject(lNatljecatelja);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.Write(noviJson);
+                }
+                Console.WriteLine(path);
+            }
+            catch
+            {
+                Console.WriteLine("Error pri pisanje u datoteku natjecatelji.json");
+            }
+           
+        }
         static void Main(string[] args)
         {
-            Korisnik natjecatelj = new Korisnik(Guid.NewGuid(), "Tin", "1234", "natjecatelj");
+            Korisnik natjecatelj = new Korisnik(Guid.NewGuid(), "Antun", "1234", "natjecatelj");
             Korisnik organizator = new Korisnik(Guid.NewGuid(), "Luka", "4321", "admin");
 
             List<Korisnik> lKorisnici = new List<Korisnik> { natjecatelj, organizator };
 
             string noviJson = JsonConvert.SerializeObject(lKorisnici);
 
-            using (StreamWriter sw = new StreamWriter(@"C:\Users\exibo\source\repos\Natjecanje u programiranju\korisnici.json"))
+            string nazivDatoteke = "korisnici.json";
+            string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, nazivDatoteke);
+
+            using (StreamWriter sw = new StreamWriter(path))
             {
                 sw.Write(noviJson);
             }
-
+            kreirajNatjecatelje();
             Izbornik();
             Console.ReadKey();
         }
